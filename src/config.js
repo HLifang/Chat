ba35e8b6494c1d1ba4a1d667cec695dd38ffcf65
HLifang <hlf_doris@163.com>
2018-11-04 13:51:21 +0800

    import chatkit and improve MessageList component

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/6c5d264b-011e-495f-b446-87d51f344ed0/token';
+
+const instanceLocator = 'v1:us1:6c5d264b-011e-495f-b446-87d51f344ed0';
+
+export { tokenUrl, instanceLocator };
